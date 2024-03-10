8f4046f50828a25f3e87cbf372cd7a50a1c79e9c
fco-gt <franomg10@gmail.com>
2024-03-10 14:40:41 -0300

    Frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,47 @@
 "use client";
 
-import React from "react";
-import { useRef, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import React, { useState, useEffect } from "react";
+
+const linkValidateRegex = /^(https?|ftp):\/\/[^\s\/$.?#].[^\s]*$/;
 
 export default function Home() {
-  const inputRef = useRef() as React.MutableRefObject<HTMLInputElement>;
-  const [shortURL, setShortURL] = useState("");
+  const [originalLink, setOriginalLink] = useState("");
+
+  const handleOriginalLink = (e: any) => {
+    const inputValue = e.target.value;
+    setOriginalLink(inputValue);
+  };
+
+  const handleFormSubmit = (e: any) => {
+    setOriginalLink("");
+  };
 
   return (
-    <main>
-     
+    <main className="flex items-center justify-center">
+      <section className="flex w-full flex-col max-w-sm items-center justify-center space-x-2 pt-32">
+        <h1>FCO-GT URL SHORTENER</h1>
+        <div className="flex gap-2 pt-11 w-full">
+          <Input
+            type="url"
+            placeholder="Paste your URL"
+            className={
+              linkValidateRegex.test(originalLink)
+                ? "text-green-500"
+                : "text-red-500"
+            }
+            value={originalLink}
+            onChange={handleOriginalLink}
+          ></Input>
+          <Button
+            onClick={handleFormSubmit}
+            disabled={linkValidateRegex.test(originalLink) ? false : true}
+          >
+            Generate
+          </Button>
+        </div>
+      </section>
     </main>
   );
 }
