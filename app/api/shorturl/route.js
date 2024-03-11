91c019996fdb1468f845b3e2a490e02cd3283e8c
fco-gt <franomg10@gmail.com>
2024-03-11 19:49:18 -0300

    Api POST working

---
+++ app/api/shorturl/route.js
@@ -0,0 +1,36 @@
+import { NextRequest, NextResponse } from "next/server";
+import { generateCode } from "../../../utils/codeGenerator";
+import Url from "../../../models/url";
+
+import connectMongoDb from "@/lib/mongoose";
+
+connectMongoDb();
+
+export const POST = async (req, { params }) => {
+  const { originalUrl } = await req.json();
+  const newCode = generateCode();
+
+  console.log(`OriginalUrl: ${originalUrl}\nNewCode: ${newCode}`);
+
+  try {
+    // Check if URL already exists (using promises)
+    const existingUrl = await Url.findOne({ originalUrl: originalUrl });
+    if (existingUrl) {
+      return NextResponse.json({ error: "URL existente" }, { status: 401 });
+    } else {
+      const newUrl = new Url({
+        originalUrl,
+        shortUrl: `http://localhost:3000/${newCode}`,
+      });
+      await newUrl.save();
+      console.log("URL Generada");
+      return NextResponse.json({ shortUrl: newCode }, { status: 201 });
+    }
+  } catch (error) {
+    console.error("Error al guardar en la base de datos: ", error);
+    return NextResponse.json(
+      { error: "Error al guardar en la base de datos" },
+      { status: 500 }
+    );
+  }
+};
--- app/page.tsx
+++ app/page.tsx
@@ -35,25 +35,25 @@ export default function Home() {
     <main className="flex items-center justify-center">
       <section className="flex w-full flex-col max-w-sm items-center justify-center space-x-2 pt-32">
         <h1>FCO-GT URL SHORTENER</h1>
-          <form onSubmit={handleFormSubmit} className="flex gap-2 pt-11 w-full">
-            <Input
-              type="url"
-              placeholder="Paste your URL"
-              className={
-                linkValidateRegex.test(originalLink)
-                  ? "text-green-500"
-                  : "text-red-500"
-              }
-              value={originalLink}
-              onChange={handleOriginalLink}
-            ></Input>
-            <Button
-              type="submit"
-              disabled={linkValidateRegex.test(originalLink) ? false : true}
-            >
-              Generate
-            </Button>
-          </form>
+        <form onSubmit={handleFormSubmit} className="flex gap-2 pt-11 w-full">
+          <Input
+            type="url"
+            placeholder="Paste your URL"
+            className={
+              linkValidateRegex.test(originalLink)
+                ? "text-green-500"
+                : "text-red-500"
+            }
+            value={originalLink}
+            onChange={handleOriginalLink}
+          ></Input>
+          <Button
+            type="submit"
+            disabled={linkValidateRegex.test(originalLink) ? false : true}
+          >
+            Generate
+          </Button>
+        </form>
       </section>
     </main>
   );
