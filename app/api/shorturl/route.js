415eae9c1142323abc77e719c4447237f1cccda5
fco-gt <franomg10@gmail.com>
2024-03-17 14:38:19 -0300

    URL Shortener V5

--- app/api/shorturl/route.js
+++ app/api/shorturl/route.js
@@ -13,19 +13,13 @@ export const POST = async (req, { params }) => {
   console.log(`OriginalUrl: ${originalUrl}\nNewCode: ${newCode}`);
 
   try {
-    // Check if URL already exists (using promises)
-    const existingUrl = await Url.findOne({ originalUrl: originalUrl });
-    if (existingUrl) {
-      return NextResponse.json({ error: "URL existente" }, { status: 401 });
-    } else {
-      const newUrl = new Url({
-        originalUrl,
-        shortUrl: `${process.env.REDIRECT_URL}${newCode}`,
-      });
-      await newUrl.save();
-      console.log("URL Generada");
-      return NextResponse.json({ shortUrl: newCode }, { status: 201 });
-    }
+    const newUrl = new Url({
+      originalUrl,
+      shortUrl: `${process.env.REDIRECT_URL}${newCode}`,
+    });
+    await newUrl.save();
+    console.log("URL Generada");
+    return NextResponse.json({ shortUrl: newCode }, { status: 201 });
   } catch (error) {
     console.error("Error al guardar en la base de datos: ", error);
     return NextResponse.json(
--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
 "use client";
 
+import React, { useState, useEffect } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import React, { useState, useEffect } from "react";
+import { title } from "@/components/primitives";
 
 const linkValidateRegex = /^(https?|ftp):\/\/[^\s\/$.?#].[^\s]*$/;
 
 export default function Home() {
   const [originalLink, setOriginalLink] = useState<string>("");
+  const [newLink, setNewLink] = useState<string>("");
 
   const handleOriginalLink = (e: React.ChangeEvent<HTMLInputElement>) => {
     setOriginalLink(e.target.value);
@@ -25,16 +27,26 @@ export default function Home() {
     });
 
     if (response.ok) {
+      const data = await response.json();
+      const code = data.shortUrl;
+      const url = `https://gt-links.vercel.app/${code}`;
+
+      setNewLink(url);
       setOriginalLink("");
+    } else if (response.status === 401) {
+      console.log("Link ya generado");
     } else {
-      console.log("A ocurrido un error");
+      console.log("Ha ocurrido un erorr");
     }
   };
 
   return (
     <main className="flex items-center justify-center">
       <section className="flex w-full flex-col max-w-sm items-center justify-center space-x-2 pt-32">
-        <h1>FCO-GT URL SHORTENER</h1>
+        <h1 className={title({ color: "green" })}>GT-Links</h1>
+        <p className="text-gray-100 text-xs mt-5">
+          *Esta app aun esta en una fase alpha*
+        </p>
         <form onSubmit={handleFormSubmit} className="flex gap-2 pt-11 w-full">
           <Input
             type="url"
@@ -54,6 +66,19 @@ export default function Home() {
             Generate
           </Button>
         </form>
+
+        <div className="mt-9 flex">
+          <strong className="flex">
+            {newLink && (
+              <p className="w-[500px] text-center">
+                Link generado:{" "}
+                <a href={newLink} target="_blank" className="text-green-500">
+                  {newLink}
+                </a>
+              </p>
+            )}
+          </strong>
+        </div>
       </section>
     </main>
   );
